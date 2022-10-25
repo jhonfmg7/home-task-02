4d4787ee5bbf33fefe3967b0d6c628d33364e24d
jhonfmg7 <jfmarmolg@unal.edu.co>
2022-10-25 12:38:16 -0500

    Resolved errors suggested by the tutor

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React from "react";
 // Components
 import Counter from "./components/Counter.tsx";
 
-const App = () => {
-    return (
-        <div className="container">
-            <h1>Hello World!</h1>
-            <Counter />
-        </div>
-    )
-};
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World!</h1>
+      <Counter />
+    </div>
+  );
+}
 
 export default App;
